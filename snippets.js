c5308941e15d0ff4b79d0bec29a6b4025149d3d2
Grae Drake <graemdrake@gmail.com>
2015-07-28 18:46:03 -0400

    Connect and main

---
+++ snippets.js
@@ -0,0 +1,49 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost/', function(err, db) {
+    if (err) {
+        console.error(err);
+        db.close();
+        return;
+    }
+    console.log('Connected to MongoDB database');
+
+    var collection = db.collection('snippets');
+
+    var create = function(name, content) {
+        db.close();
+    };
+
+    var read = function(name) {
+        db.close();
+    };
+
+    var update = function(name, content) {
+        db.close();
+    };
+
+    var del = function(name, content) {
+        db.close();
+    };
+
+    var main = function() {
+        if (process.argv[2] == 'create') {
+            create(process.argv[3], process.argv[4]);
+        }
+        else if (process.argv[2] == 'read') {
+            read(process.argv[3]);
+        }
+        else if (process.argv[2] == 'update') {
+            update(process.argv[3], process.argv[4]);
+        }
+        else if (process.argv[2] == 'delete') {
+            del(process.argv[3]);
+        }
+        else {
+            console.error('Command not recognized');
+            db.close();
+        }
+    };
+
+    main();
+});
